7b52ce0e418623b0ab0717f24390b1dc1ad1b13a
mfokins <75252301+mfokins@users.noreply.github.com>
2021-03-02 12:05:06 +0100

    TableStarter
    TemporaryStuff

--- src/view/ControllerTemperatureList.java
+++ src/view/ControllerTemperatureList.java
@@ -5,20 +5,20 @@ import javafx.scene.layout.Region;
 import viewmodel.MainWindowViewModel;
 import viewmodel.TemperatureListViewModel;
 
-public class ControllerTemperatureList {
-
+public class ControllerTemperatureList
+{
 
   // @FXML private ListView<String> temperatureList;
   private Region root;
   private ViewHandler viewHandler;
   private TemperatureListViewModel temperatureListViewModel;
 
-  public void init(ViewHandler viewHandler, TemperatureListViewModel temperatureListViewModel, Region root)
+  public void init(ViewHandler viewHandler,
+      TemperatureListViewModel temperatureListViewModel, Region root)
   {
     this.viewHandler = viewHandler;
     this.temperatureListViewModel = temperatureListViewModel;
     this.root = root;
-
 
     //this.temperatureList.setItems(temperatureListViewModel.getList());
 
--- src/viewmodel/TemperatureListViewModel.java
+++ src/viewmodel/TemperatureListViewModel.java
@@ -1,15 +1,41 @@
 package viewmodel;
 
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import model.TemperatureModel;
 
-public class TemperatureListViewModel
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class TemperatureListViewModel implements PropertyChangeListener
 {
   private TemperatureModel temperatureModel;
+  private ObservableList<String> temperatureList;
 
   public TemperatureListViewModel(TemperatureModel model)
   {
     this.temperatureModel = model;
+    this.temperatureList = FXCollections.observableArrayList();
+    this.temperatureModel.addListener(null, this);
 
+  }
+
+  @Override public void propertyChange(PropertyChangeEvent evt)
+  {
+    switch (evt.getPropertyName())   // <--- thermometer id
+    {
+      case "outside":
+        Platform.runLater(() -> outsideThermometerList.add(evt.getNewValue().toString()));
+        break;
+      case "closest":
+        Platform
+            .runLater(() -> insideHeaterThermometerList.add(evt.getNewValue().toString()));
+        break;
+      case "furthest":
+        Platform.runLater(() -> insideThermometerList.add(evt.getNewValue().toString()));
+        break;
+    }
 
   }
 }
