561209ece6f317f84eee65e009349b7f2a1f1e63
mfokins <75252301+mfokins@users.noreply.github.com>
2021-03-02 11:41:34 +0100

    onBack action + temperatureList?
    temperatureList is made as a comment yet

--- src/view/ControllerTemperatureList.java
+++ src/view/ControllerTemperatureList.java
@@ -1,11 +1,14 @@
 package view;
 
+import javafx.event.ActionEvent;
 import javafx.scene.layout.Region;
 import viewmodel.MainWindowViewModel;
 import viewmodel.TemperatureListViewModel;
 
 public class ControllerTemperatureList {
 
+
+  // @FXML private ListView<String> temperatureList;
   private Region root;
   private ViewHandler viewHandler;
   private TemperatureListViewModel temperatureListViewModel;
@@ -15,6 +18,9 @@ public class ControllerTemperatureList {
     this.viewHandler = viewHandler;
     this.temperatureListViewModel = temperatureListViewModel;
     this.root = root;
+
+
+    //this.temperatureList.setItems(temperatureListViewModel.getList());
 
     //bindings
   }
@@ -27,5 +33,10 @@ public class ControllerTemperatureList {
   public Region getRoot()
   {
     return root;
+  }
+
+  public void onBack(ActionEvent actionEvent)
+  {
+    this.viewHandler.openView("main");
   }
 }
