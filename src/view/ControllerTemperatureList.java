2a2a2155894a94838e38543a0543443ebbdfe278
Alex <abistrovs@gmail.com>
2021-03-03 10:43:27 +0100

    view + controller update

--- src/view/ControllerTemperatureList.java
+++ src/view/ControllerTemperatureList.java
@@ -1,17 +1,27 @@
 package view;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.Region;
 import model.Temperature;
 import viewmodel.MainWindowViewModel;
 import viewmodel.TemperatureListViewModel;
+import viewmodel.TemperatureViewModel;
 
 import java.util.ArrayList;
 
 public class ControllerTemperatureList
 {
 
-  // @FXML private ListView<String> temperatureList;
+  //@FXML private ListView<String> temperatureList;
+  @FXML private TableView<TemperatureViewModel> temperatureTable;
+  @FXML private TableColumn<TemperatureViewModel, String> nameColumn;
+  @FXML private TableColumn<TemperatureViewModel, String> temperatureColumn;
+  @FXML private TableColumn<TemperatureViewModel, String> timeDateColumn;
+
   private Region root;
   private ViewHandler viewHandler;
   private TemperatureListViewModel temperatureListViewModel;
